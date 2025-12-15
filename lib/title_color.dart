849c0af0228ba7b15b8522d090abf2e6fbdd74c6
Yasathilaka M Kiridana <kavindakiridena@gmail.com>
2025-12-15 16:58:16 +0530

    Update title_color.dart

--- lib/title_color.dart
+++ lib/title_color.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class TitleColor {
-  final String title;
-  final Color color;
-  final Color backcolor;
+  String title;
+  Color color;
+  Color backcolor;
   TitleColor(this.title, this.color, this.backcolor);
 }
